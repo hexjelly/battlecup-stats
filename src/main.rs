6549e5e59e5e91acd1b7e4b1bb4f971a45bb16f6
Roger Andersen <hexjelly@hexjelly.com>
2017-01-16 07:20:25 +0100

    feat: add Clap for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 #![feature(plugin)]
 #![plugin(maud_macros)]
+
+#[macro_use]
+extern crate clap;
 extern crate maud;
 
 mod fetch;
 mod template;
 
+use std::collections::HashMap;
+
 use fetch::*;
 use template::template;
 
+use clap::App;
+
 fn main() {
-    let _ = get_battle_result();
-    println!("{}", template().into_string());
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let mut battle_data = HashMap::new();
+    battle_data.insert(666, "test :^)".to_string());
+    let _ = get_battle_result(666);
+    println!("{}", template("test?", &battle_data).into_string());
 }
