d24c476ef250316b3a3df38909ef1c65e6e20006
Roger Andersen <hexjelly@hexjelly.com>
2017-01-16 08:33:54 +0100

    feat: parse argument list from &str to int

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,21 @@ mod template;
 
 use std::collections::HashMap;
 
-use fetch::*;
+use fetch::get_battle_result;
 use template::template;
 
 use clap::App;
 
 fn main() {
+    // arg parsing
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+    let title = matches.value_of("title").unwrap_or("Battle Cup Stats");
+    // split comma separated list, then convert &str to get battle id list as ints
+    let battle_id_list: Vec<_> = matches.value_of("INPUT").unwrap().split(',').map(|x| x.parse::<u32>().unwrap()).collect();
 
     let mut battle_data = HashMap::new();
     battle_data.insert(666, "test :^)".to_string());
     let _ = get_battle_result(666);
-    println!("{}", template("test?", &battle_data).into_string());
+    println!("{:?} - {}", battle_id_list, template(&title, &battle_data).into_string());
 }
